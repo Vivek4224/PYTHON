cd1b72bf2afbe64a9f10742bdea6f0a017a056e9
Vivek4224 <vivekbarvaliya4224@gmail.com>
2024-08-14 06:44:35 +0530

    add

--- CORE/Task/task_pattern.py
+++ CORE/Task/task_pattern.py
@@ -1,14 +1,14 @@
 num = 9
-for row in range(1, num + 1):
-    for col in range(1, row + 1):
-        if row % 2 == 0:
-            if col == 1:
-                print("*", end=' ')
-                continue
-            print(" ", end=' ')
-        else:
-            print("*", end=' ')
-    print()
+# for row in range(1, num + 1):
+#     for col in range(1, row + 1):
+#         if row % 2 == 0:
+#             if col == 1:
+#                 print("*", end=' ')
+#                 continue
+#             print(" ", end=' ')
+#         else:
+#             print("*", end=' ')
+#     print()
 for row in range(1,num+1):
     for col in range(1,num+1):
         if col % 2 == 0:
