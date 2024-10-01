f820ff93e8156ad7b2bcb7bef32ca123d1945491
Vivek4224 <vivekbarvaliya4224@gmail.com>
2024-10-01 17:31:57 +0530

    add

--- Advance/GUI/myDatabase/checkCredentials.py
+++
@@ -1,8 +0,0 @@
-from .dbConnection import db, cursor
-
-def check_credentials(email, password):
-    # Use a parameterized query to prevent SQL injection
-    cursor.execute("SELECT * FROM students WHERE email=%s AND password=%s", (email, password))
-    user = cursor.fetchone()  # Fetch one user record
-    cursor.close()  # Close the cursor
-    return user is not None  # Return True if user exists, else False
--- Advance/GUI/myDatabase/dbConnection.py
+++ Advance/GUI/myDatabase/dbConnection.py
@@ -1,10 +1,10 @@
 import mysql.connector
 
 db = mysql.connector.connect(
-    user = 'root',
-    password = 'Vivek@2003',
-    host = '127.0.0.1',
-    database = ''
+    user='root', 
+    password='Vivek@2003',  
+    host='127.0.0.1',
+    database='Vivekdb'
 )
 
 cursor = db.cursor()
